488480d4ed5d9dec0f852dba4b5b5a35048908c2
André Luiz <andre.luiz.boni.bezerra@gmail.com>
2023-11-10 09:23:24 -0300

    Improves styles on button

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -13,6 +13,12 @@ export const Button = styled.button`
   cursor: pointer;
   transition: background 250ms ease;
   will-change: background;
+  transition: all 250ms ease;
+
+  &:focus {
+    outline: var(--brand-color-primary-4);
+    box-shadow: 0px 20px 20px 0px rgba(0, 0, 0, 0.2);
+  }
 
   &:hover {
     background-color: var(--brand-color-primary-4);
